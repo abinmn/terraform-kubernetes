93c8ecf3fa8be58b2df93611b20f3712f76d77d8
Abin Mn <abinmn@users.noreply.github.com>
2021-11-16 19:06:03 +0530

    REFACTOR backend resources into seperate directory

---
+++ backend/variables.tf
@@ -0,0 +1,11 @@
+variable "AWS_REGION" {
+  default = "ap-south-1"
+}
+
+variable "TERRAFORM_STATE_BUCKET" {
+  default = "tf-state-mn"
+}
+
+variable "TERRAFORM_STATE_DB" {
+  default = "tf-state-db-mn"
+}
